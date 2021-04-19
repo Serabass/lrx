bb78aab0f962ee220912da81cf2e4c95f13d6871
Serabass <serabass565@gmail.com>
2021-04-20 00:56:50 +0600

    upd

--- src/LRX.tsx
+++ src/LRX.tsx
@@ -4,30 +4,66 @@ import React from "react";
 import parser from './parser.pegjs';
 import './lrx.sass';
 
+interface LRXDocument {
+    title: LRXDocumentTitle;
+    blocks: LRXDocumentBlock[]
+}
+
+interface LRXDocumentTitle {
+    title: string;
+}
+
+interface LRXDocumentBlock {
+    header: {
+        title: string;
+    };
+    body: LRXGeneralLine[];
+}
+
+interface LRXGeneralLine {
+    type: string;
+}
+
+interface LRXChordSpace {
+    start: string;
+    end: string;
+}
+
+interface LRXChord {
+    space: LRXChordSpace;
+    note: string;
+    mod: string;
+    suffix: string;
+}
+
+interface LRXChordsLine {
+    type: 'CHORDS_LINE';
+    chords: LRXChord[];
+}
+
 const LRX = ({contents}: any) => {
-    let lrxDoc = parser.parse(contents.replace(/^\s+/));
+    let lrxDoc: LRXDocument = parser.parse(contents.replace(/^\s+/));
     return <pre className="lrx-document">
         <h1>{lrxDoc.title.title}</h1>
 
-        {lrxDoc.blocks.map((block: any, i: number) => <LRXBlock block={block} key={i} />)}
-        {JSON.stringify(lrxDoc, null, 4)}
+        {lrxDoc.blocks.map((block, i) => <LRXBlock block={block} key={i} />)}
     </pre>;
 };
 
-export function LRXBlock({block}: any) {
-    return <div>
-        <h3>[{block.header.title}]</h3>
-        {block.body.map((line: any, i: number) => <LRXLine line={line} key={i} />)}
+export function LRXBlock({block}: { block: LRXDocumentBlock }) {
+    return <div className="lrx-block">
+        <h3 className="lrx-block-header">[{block.header.title}]</h3>
+        {block.body.map((line, i) => <LRXLine line={line} key={i} />)}
     </div>
 }
 
-export function LRXLine({line}: any) {
+export function LRXLine({line}: { line: LRXGeneralLine }) {
     switch (line.type) {
         case 'EMPTY_LINE':
-            return <p></p>;
+            return <p />;
 
         case 'CHORDS_LINE':
-            return <LRXChordLine line={line} />;
+            return <LRXChordLine line={line as LRXChordsLine} />;
 
         case 'LINE':
             return <LRXLyricsLine line={line} />;
@@ -37,9 +73,9 @@ export function LRXLine({line}: any) {
     }
 }
 
-export function LRXChordLine({line}: any) {
+export function LRXChordLine({line}: { line: LRXChordsLine }) {
     return <p>
-        {line.chords.map((chord: any, i: number) => <span className="chord" key={i}>
+        {line.chords.map((chord, i: number) => <span className="chord" key={i}>
             {chord.space.start}{chord.note}{chord.mod || ''}{chord.suffix || ''}{chord.space.end}
         </span>)}
     </p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import { render } from "react-dom";
 import App from "./App";
 import LRX from './LRX';
 
-const root = document.getElementById("root");
-render(<App />, root);
-
 document.querySelectorAll('script[type="text/lrx"]').forEach((el) => {
     let contents = el.innerHTML;
     let pre = document.createElement('pre');
