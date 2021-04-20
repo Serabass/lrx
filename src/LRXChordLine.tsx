5f02b8747bcc1613328d95e9f8219391a93f1a9e
Serabass <serabass565@gmail.com>
2021-04-20 10:07:10 +0600

    upd

--- src/LRXChordLine.tsx
+++ src/LRXChordLine.tsx
@@ -1,32 +1,10 @@
 import React from "react";
 import { LRXChordsLine } from "./types";
+import { transposeChord } from "./transpose-chord";
 
 export interface LRXChordLineProps {
   line: LRXChordsLine;
   transpose?: number;
-}
-
-function transposeChord(chord: string, amount: number) {
-  // https://stackoverflow.com/a/45979883/2468733
-  let scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-  let normalizeMap: any = {
-    Cb: "B",
-    Db: "C#",
-    Eb: "D#",
-    Fb: "E",
-    Gb: "F#",
-    Ab: "G#",
-    Bb: "A#",
-    "E#": "F",
-    "B#": "C"
-  };
-  return chord.replace(/[CDEFGAB](b|#)?/g, function (match) {
-    let i =
-      (scale.indexOf(normalizeMap[match] ? normalizeMap[match] : match) +
-        amount) %
-      scale.length;
-    return scale[i < 0 ? i + scale.length : i];
-  });
 }
 
 export function LRXChordLine({ line, transpose = 0 }: LRXChordLineProps) {
