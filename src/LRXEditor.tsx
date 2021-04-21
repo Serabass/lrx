84237ecef7dc7b63a6a4c79941036d4c5044e308
Serabass <serabass565@gmail.com>
2021-04-22 00:27:32 +0600

    upd

--- src/LRXEditor.tsx
+++ src/LRXEditor.tsx
@@ -5,7 +5,8 @@ import { LRXDocument } from "./types";
 // @ts-ignore
 import parser from "./parser.pegjs";
 import { hot } from "react-hot-loader";
-import { UnControlled as CodeMirror } from "react-codemirror2";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import { Editor } from "codemirror";
 
 export interface LRXEditorProps {
   source: string;
@@ -14,64 +15,58 @@ export interface LRXEditorProps {
 
 const LRXEditor = ({ source, audioUrl }: LRXEditorProps) => {
   let [sourceText, setSourceText] = useState(source);
-  let src: string, lrxDoc;
-  [sourceText, setSourceText] = useState(source);
-  src = sourceText.replace(/^\s+/, "");
-  let Editor = ({ src }: { src: string }) => {
-    return (
-      <CodeMirror
-        value={src}
-        options={{
-          mode: "xml",
-          theme: "material",
-          lineNumbers: true
-        }}
-        onChange={(editor, data, value) => {
-          setSourceText(value);
-        }}
-      />
-    );
-  };
+  let [editor, setEditor] = useState<Editor>();
+  let src = sourceText.replace(/^\s+/, "");
+  let ref = React.createRef();
 
-  try {
-    lrxDoc = parser.parse(src) as LRXDocument;
-
-    return (
-      <Row className="lrx-editor">
-        <Col md={12}>
-          <Editor src={src} />
-        </Col>
-        <Col md={12}>
-          <LRX doc={lrxDoc} audioUrl={audioUrl} />
-        </Col>
-      </Row>
-    );
-  } catch (e) {
-    return (
-      <Row className="lrx-editor">
-        <Col md={12}>
-          <Editor src={src} />
-        </Col>
-        <Col md={12}>
-          <Alert
-            message={
-              <pre>
-                <p>{e.message}</p>
-                <p>at</p>
-                <p>
-                  {e.location.start.line}:{e.location.start.column}
-                </p>
-                <p>
-                  {e.location.end.line}:{e.location.end.column}
-                </p>
-              </pre>
-            }
-            type="error"
-          />
-        </Col>
-      </Row>
-    );
-  }
+  return (
+    <Row className="lrx-editor">
+      <Col md={12}>
+        <CodeMirror
+          ref={() => ref}
+          value={src}
+          options={{
+            mode: "lrx",
+            theme: "material",
+            lineNumbers: true
+          }}
+          onRenderLine={(editor, line, element) => {
+            setEditor(editor);
+          }}
+          onBeforeChange={(editor, data, value) => {
+            setSourceText(value);
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+      </Col>
+      <Col md={12}>
+        {(() => {
+          try {
+            let lrxDoc = parser.parse(src) as LRXDocument;
+            return <LRX doc={lrxDoc} audioUrl={audioUrl} />;
+          } catch (e) {
+            return (
+              <Alert
+                message={
+                  <pre>
+                    <p>{e.message}</p>
+                    <p>at</p>
+                    <p>
+                      {e.location.start.line}:{e.location.start.column}
+                    </p>
+                    <p>
+                      {e.location.end.line}:{e.location.end.column}
+                    </p>
+                  </pre>
+                }
+                type="error"
+              />
+            );
+          }
+        })()}
+      </Col>
+    </Row>
+  );
 };
 
 export default hot(module)(LRXEditor);
